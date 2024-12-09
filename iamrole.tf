e3cdde7a7320a1889243b3fc9a87b55a063fd4bb
nguyenvankhanhmhb <nguyenvankhanhmhb@gmail.com>
2024-12-09 20:57:01 +0700

    IAM ROLE

--- iamrole.tf
+++ iamrole.tf
@@ -1,34 +1,26 @@
-# Tạo IAM Role
-resource "aws_iam_role" "elasticbeanstalk_instance_role" {
-  name = "aws-elasticbeanstalk-ec2-role"
+resource "aws_iam_role" "elasticbeanstalk_service_role" {
+  name = "elasticbeanstalk-service-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
+        Effect = "Allow"
         Principal = {
-          Service = "ec2.amazonaws.com"
+          Service = "elasticbeanstalk.amazonaws.com"
         }
-      },
+        Action = "sts:AssumeRole"
+      }
     ]
   })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkEnhancedHealth",
+    "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService"
+  ]
 }
 
-# Gán policy cho IAM Role
-resource "aws_iam_role_policy_attachment" "elasticbeanstalk_instance_role_policy" {
-  role       = aws_iam_role.elasticbeanstalk_instance_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
-}
-
-resource "aws_iam_role_policy_attachment" "elasticbeanstalk_instance_role_policy_s3" {
-  role       = aws_iam_role.elasticbeanstalk_instance_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
-# Tạo IAM Instance Profile và gán IAM Role vào đó
 resource "aws_iam_instance_profile" "elasticbeanstalk_instance_profile" {
-  name = "aws-elasticbeanstalk-ec2-instance-profile"
-  role = aws_iam_role.elasticbeanstalk_instance_role.name
+  name = "elasticbeanstalk-instance-profile"
+  role = aws_iam_role.elasticbeanstalk_service_role.name
 }
